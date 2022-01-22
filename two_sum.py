f421433335ab761bfa9cc4ea9135f48b8e792f98
AlfyorovaAlyona <alfyorova.av@phystech.edu>
2022-01-22 15:07:00 +0200

    Two sum problem

--- two_sum.py
+++ two_sum.py
@@ -6,7 +6,7 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-
+        return []
 
 def print_hi(name):
     print(f'Hi, {name}')
