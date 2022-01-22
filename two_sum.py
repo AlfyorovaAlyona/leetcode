e6827917b326a6418a714a72e9dd0d9b082998ee
AlfyorovaAlyona <alfyorova.av@phystech.edu>
2022-01-22 16:55:30 +0200

    Two sum solution

--- two_sum.py
+++ two_sum.py
@@ -7,10 +7,18 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return []
-
+    def two_sum_hash(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in range(len(nums)):
+            dictionary[nums[i]] = i
+        for j in range(len(nums)):
+            num = target - nums[j]
+            if num in dictionary.keys() and dictionary[num] != j:
+                return [j, dictionary[num]]
+        return []
 
 if __name__ == '__main__':
-    nums = [3, 2, 4]
+    nums = [3,2,4]
     target = 6
     sol = Solution()
-    print(sol.two_sum(nums, target))
+    print(sol.two_sum_hash(nums, target))
