ecd5fb29a46b329d9b15ee8b0771d19e8a7b1c66
AlfyorovaAlyona <alfyorova.av@phystech.edu>
2022-01-22 12:47:20 +0200

    rename

---
+++ two_sum.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 4]
+    target = 6
+    sol = Solution()
+    print(sol.two_sum(nums, target))
